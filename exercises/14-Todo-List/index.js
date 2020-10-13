7df18f2af066b210ab7f738b4071ef6ccd7412f7
amafjarkasi <amafjarkasi@gmail.com>
2020-10-13 18:47:48 +0000

    latest updates

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,5 +1,29 @@
 // Your code here
 
-let addTask = document.getElementById("addToDo");
-add
-addTask.
+const addTask = document.querySelector("#addToDo");
+const removeTask = document.querySelectorAll(".fa-trash");
+
+console.log(removeTask);
+
+addTask.addEventListener("keypress", function(event) {
+	console.log(event.key);
+	if (event.key == "Enter") {
+		const taskInput = event.target.value;
+		console.log(taskInput);
+		const newListItem = document.createElement("li");
+		console.log(newListItem);
+		const getListItem = document.querySelector("ul");
+		const htmlAddition = `<span><i class="fa fa-trash"></i></span> ${taskInput}`;
+		newListItem.innerHTML = htmlAddition;
+		getListItem.appendChild(newListItem);
+	}
+});
+
+removeTask.forEach(element => {
+	console.log(element);
+	element.addEventListener("click", function(event) {
+		let iconSelected = event.target.parentNode.parentNode;
+		const getListItem = document.querySelector("ul");
+		getListItem.removeChild(iconSelected);
+	});
+});
